b4198b57ac2acdc4826bcdb718f7b26b999e5f14
taahirahismail <taahirah.i@gmail.com>
2023-08-16 13:47:07 +0200

    users done

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const {express, routes} = require('./controllers');
+const path = require('path');
+const port = +process.env.PORT || 3000;
+
+// static
+app.use(express.static('./static'));
+app.use(
+    express.urlencoded({
+        extended: false
+    }),
+    routes
+)
+
+routes.get('^/$|/challenger',
+(req, res) => {
+    res.sendFile(path.resolve(__dirname, './static/html/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+})
---
+++ api/models/index.js
@@ -0,0 +1,9 @@
+const Users = require("./users");
+// const Orders = require("./orders");
+// const Books = require("./books");
+// const BookAuthors = require('./bookAuthors');
+
+// export all objects
+module.exports = {
+    users: new Users(),
+}
